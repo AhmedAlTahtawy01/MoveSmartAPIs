144df648f4aafcf48b515133669d3b383832b160
kamalemad2002 <kamalemad2002@gmail.com>
2025-03-13 21:26:09 +0200

    update main branch with last 8 DAL Classes

---
+++ Move_Smart/Move_Smart/Controllers/SparePartController.cs
@@ -0,0 +1,56 @@
+﻿using DataAccessLayer.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Move_Smart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SparePartController : ControllerBase
+    {
+        private readonly Sparepart _isparepart;
+
+        public SparePartController(Sparepart isparepart)
+        {
+            _isparepart = isparepart;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllSparePart()
+        {
+            var data = await _isparepart.GetAllSparePart();
+            return Ok(data);
+        }
+        [HttpGet("{PartName}")]
+        public async Task<IActionResult> GetSparePartByName(string PartName)
+        {
+            var data = await _isparepart.GetSparePartByName(PartName);
+            return Ok(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddSparePart([FromBody] Sparepart spare)
+        {
+             await _isparepart.AddSparePart(spare);
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateSparePart([FromBody] Sparepart spare)
+        {
+             await _isparepart.UpdateSparePart(spare);
+            return Ok();
+        }
+        [HttpDelete]
+        [Route("{PartName}")]
+        public async Task<IActionResult> DeleteSparePart(string PartName)
+        {
+            await _isparepart.DeleteSparePart(PartName);
+            return Ok();
+        }
+        [HttpPut("{PartName}")]
+        public async Task<IActionResult> UpdateByNameSparePart(string PartName, Sparepart spare)
+        {
+            await _isparepart.UpdateByNameSparePart(PartName, spare);
+            return Ok();
+        }
+
+    }
+}
--- Move_Smart/Move_Smart/Program.cs
+++ Move_Smart/Move_Smart/Program.cs
@@ -1,3 +1,7 @@
+using DataAccessLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<appDBContext>(options => options.UseMySql(connectionString, ServerVersion.Parse("8.0.36-mysql")));
+
+builder.Services.AddScoped<Sparepart>();
+
+
+var options = new JsonSerializerOptions
+{
+    IncludeFields = true
+};
 
 var app = builder.Build();
 
@@ -15,8 +29,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
