73384fe301ed9b26878510b4ba9369b4ca43abdd
AhmedHamdy <ahmedaltahtawy01@gmail.com>
2025-05-14 17:52:08 +0300

    Adding CORS policy

--- Move_Smart/Move_Smart/Program.cs
+++ Move_Smart/Move_Smart/Program.cs
@@ -205,17 +205,31 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.IncludeFields = true;
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+
+
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
