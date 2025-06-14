2251d1faa35f1230605949422727047588ea83f1
AhmedHamdy <ahmedaltahtawy01@gmail.com>
2025-06-14 06:04:36 +0300

    Restore edits

--- Move_Smart/Move_Smart/Program.cs
+++ Move_Smart/Move_Smart/Program.cs
@@ -266,10 +266,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
-        policy.WithOrigins("http://127.0.0.1:5500")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
     });
 });
 
@@ -284,7 +283,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Move Smart API V1");
-        //c.RoutePrefix = string.Empty; // <--- Swagger is now at root
+        c.RoutePrefix = string.Empty; // <--- Swagger is now at root
     });
 }
 
